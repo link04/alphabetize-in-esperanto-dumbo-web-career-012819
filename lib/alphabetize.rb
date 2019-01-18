c11e9167ae9d40248b263c3941477e47c2930096
Maximo Bautista <max.bautista97@gmail.com>
2019-01-18 17:49:34 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,10 @@
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+  esperanto = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  arr.sort_by do |sentence|
+    sentence.split("").collect do |character|
+      esperanto.index(character)
+    end
+  end
 end
